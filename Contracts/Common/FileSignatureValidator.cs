e9ed4b60c2d4c00d4ab03723e18aecfafbebe038
Ziad Yahya <contact@zyahya.dev>
2026-09-09 13:57:06 +0300

    Rename validation method in FileSignatureValidator to ensure only non-blocked file signatures are accepted

--- Contracts/Common/FileSignatureValidator.cs
+++ Contracts/Common/FileSignatureValidator.cs
@@ -5,12 +5,12 @@ public class FileSignatureValidator : AbstractValidator<IFormFile>
     public FileSignatureValidator()
     {
         RuleFor(x => x)
-            .Must(BeAllowedSignature)
+            .Must(BeNotBlockedSignature)
             .When(x => x != null)
             .WithMessage("File type is not allowed.");
     }
 
-    private static bool BeAllowedSignature(IFormFile file)
+    private static bool BeNotBlockedSignature(IFormFile file)
     {
         var binary = new BinaryReader(file.OpenReadStream());
         var bytes = binary.ReadBytes(2);
