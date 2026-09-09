94b82c61d784805079c24283bbabb72fa63d77f0
Ziad Yahya <contact@zyahya.dev>
2026-09-09 13:56:10 +0300

    Add allowed image signatures and extensions to FileSettings

--- Settings/FileSettings.cs
+++ Settings/FileSettings.cs
@@ -5,4 +5,7 @@ public static class FileSettings
     public const int MaxSizeInMB = 1;
     public const int MaxSizeInBytes = MaxSizeInMB * 1024 * 1024;
     public static readonly string[] BlockedSignatures = ["4d-5a", "2f-2a", "d0-cf", "63-6f"];
+
+    public static readonly string[] AllowedImageSignatures = ["ff-d8", "89-50"];
+    public static readonly string[] AllowedImageExtensions = [".jpg", ".jpeg", ".png"];
 }
