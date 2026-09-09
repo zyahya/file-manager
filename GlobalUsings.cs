99fbf21877c6e3ba8fc50feb2615ff11922b6abd
Ziad Yahya <contact@zyahya.dev>
2026-09-09 11:25:03 +0300

    Update and sort global usings

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,6 +1,12 @@
 global using FileManager;
+global using FileManager.Contracts;
+global using FileManager.Contracts.Common;
+global using FileManager.Entities;
 global using FileManager.Services;
+global using FileManager.Settings;
+
+global using FluentValidation;
+
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
-global using FileManager.Entities;
