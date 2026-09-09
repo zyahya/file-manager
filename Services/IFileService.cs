3cd469c564e46bdd31cede764defe1f458be5e92
Ziad Yahya <contact@zyahya.dev>
2026-09-09 11:39:01 +0300

    Add default value for CancellationToken in UploadAsync and UploadMultipleAsync methods

--- Services/IFileService.cs
+++ Services/IFileService.cs
@@ -2,7 +2,7 @@ namespace FileManager.Services;
 
 public interface IFileService
 {
-    Task<Guid> UploadAsync(IFormFile file, CancellationToken ct);
+    Task<Guid> UploadAsync(IFormFile file, CancellationToken ct = default);
 
-    Task<IEnumerable<Guid>> UploadMultipleAsync(IFormFileCollection files, CancellationToken ct);
+    Task<IEnumerable<Guid>> UploadMultipleAsync(IFormFileCollection files, CancellationToken ct = default);
 }
