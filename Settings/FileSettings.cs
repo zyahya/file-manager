b02cdc894cb5756b6fee8dac4abfc3236613b102
Ziad Yahya <contact@zyahya.dev>
2026-09-09 11:07:10 +0300

    Add validation on file signature, file names and fix the validator name from 'UploadFileValidator' to 'UploadFileRequestValidator'

--- Settings/FileSettings.cs
+++ Settings/FileSettings.cs
@@ -4,4 +4,5 @@ public static class FileSettings
 {
     public const int MaxSizeInMB = 1;
     public const int MaxSizeInBytes = MaxSizeInMB * 1024 * 1024;
+    public static readonly string[] BlockedSignatures = ["4d-5a", "2f-2a", "d0-cf", "63-6f"];
 }
---
+++ Validators/UploadFileRequestValidator.cs
@@ -0,0 +1,48 @@
+using FileManager.Contracts;
+using FileManager.Settings;
+using FluentValidation;
+
+namespace FileManager.Validators;
+
+public class UploadFileRequestValidator : AbstractValidator<UploadFileRequest>
+{
+    public UploadFileRequestValidator()
+    {
+        RuleFor(x => x.File)
+            .Must(file => file.Length <= FileSettings.MaxSizeInBytes)
+            .When(x => x.File != null)
+            .WithMessage($"File size must not exceed {FileSettings.MaxSizeInMB} MB.");
+
+        RuleFor(x => x.File)
+            .Must(BeAllowedSignature)
+            .When(x => x.File != null)
+            .WithMessage("File type is not allowed.");
+
+        RuleFor(x => x.File)
+            .Must(file => HasInvalidFileNameChars(file.FileName))
+            .When(x => x.File != null)
+            .WithMessage("The file name contains invalid characters.");
+    }
+
+    private static bool BeAllowedSignature(IFormFile file)
+    {
+        var binary = new BinaryReader(file.OpenReadStream());
+        var bytes = binary.ReadBytes(2);
+        var fileSignatureHex = BitConverter.ToString(bytes);
+
+        foreach (var signature in FileSettings.BlockedSignatures)
+        {
+            if (fileSignatureHex.Equals(signature, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool HasInvalidFileNameChars(string fileName)
+    {
+        return fileName.IndexOfAny(Path.GetInvalidFileNameChars()) == -1;
+    }
+}
